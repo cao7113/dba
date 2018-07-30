b204a71618621a022ec9699fd3c1ab61bc1782ee
Ruijian Cao <cao7113@hotmail.com>
2018-07-30 18:22:10 +0800

    fix

--- ops/clean-public-schema.rb
+++ ops/clean-public-schema.rb
@@ -10,6 +10,7 @@ ar_internal_metadata
 schema_migrations
 )
 
+## stats
 tables = db.tables - reserved_tables
 puts "==reserver #{reserved_tables.size} tables in schema: #{schema}"
 puts reserved_tables.map(&:to_s).join(' ')
@@ -18,3 +19,9 @@ puts "==will archive #{tables.size} tables"
 tables.sort.each_with_index do |t, idx|
   puts [idx + 1, t, db.from(t).count].join ' '
 end
+
+## archiv db
+# pga fork starup_production xxx/agentcloud_legacy201808 --dump-opts='--schema public'
+
+
+## danage clean tables
