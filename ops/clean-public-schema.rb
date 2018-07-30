f99e4258049c2232023365e71d253f06aaa89115
Ruijian Cao <cao7113@hotmail.com>
2018-07-30 19:23:19 +0800

    fix

--- ops/clean-public-schema.rb
+++ ops/clean-public-schema.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env dbcli runsqlet
 require 'byebug'
+require 'tty-prompt'
+prompt = TTY::Prompt.new
 
 db = conn.sequel_db
 schema = ENV['SCHEMA'] || :public
@@ -24,3 +26,11 @@ end
 # pga fork starup_production xxx/agentcloud_legacy201808 --dump-opts='--schema public'
 
 ## danage!! clean tables
+if prompt.yes?("Are you sure to drop tables?")
+  tables.each_with_index do |t, idx|
+    puts "==clean table: #{t}"
+    db.run "drop table if exists #{t} cascade;"
+  end
+end
+
+puts "world is clean!"
