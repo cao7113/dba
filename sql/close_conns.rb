05124b97b9cb95f2929b6093b8cc191f1aa8135a
Ruijian Cao <cao7113@hotmail.com>
2018-11-20 19:11:02 +0800

    add createdb and dropdb command

--- sql/close_conns.rb
+++ sql/close_conns.rb
@@ -3,7 +3,7 @@
 # Query pg_stat_activity and get the pid values you want to kill, then issue SELECT pg_terminate_backend(pid int) to them, apply 9.2+
 sql = <<-SQL
   SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '#{conn.dbname}' AND pid <> pg_backend_pid();
-  select pid, usename, application_name, client_addr, client_hostname, client_port backend_start from pg_stat_activity where datname = '#{conn.dbname}';
+  select pid, usename, application_name, client_addr, client_hostname, client_port, backend_start from pg_stat_activity where datname = '#{conn.dbname}';
 SQL
 r = conn.run_sql 
 puts r.output
